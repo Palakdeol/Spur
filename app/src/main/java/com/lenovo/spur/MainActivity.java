832d70777e93f84116f080052979b2b1bf8f7f32
Palakdeol <deolpalak@gmail.com>
2018-03-15 14:27:04 +0530

    First commit

---
+++ app/src/main/java/com/lenovo/spur/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lenovo.spur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
